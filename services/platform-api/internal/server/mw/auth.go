5675f46836e30ba34a296f79b738d36ff9fbabfc
carlosmsanchezm <carlos.sanchez.m.m@gmail.com>
2025-12-05 14:59:53 -0500

    work for backend work before frontend connection

--- services/platform-api/internal/server/mw/auth.go
+++ services/platform-api/internal/server/mw/auth.go
@@ -200,7 +200,7 @@ func (a *Authenticator) Authenticate(ctx context.Context, token string, meta Req
 		jwt.WithValidMethods([]string{"RS256", "RS384", "RS512"}),
 	}
 	if len(aud) > 0 {
-		parserOpts = append(parserOpts, jwt.WithAudience(aud...))
+		parserOpts = append(parserOpts, jwt.WithAudience(aud[0]))
 	}
 
 	t, err := jwt.ParseWithClaims(token, claims, func(tok *jwt.Token) (interface{}, error) {
